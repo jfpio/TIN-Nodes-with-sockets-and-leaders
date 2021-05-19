b27dc9b80c94bc916df5e297c09802bfc3664e8b
dchmiel1 <d.chmielewiec@stud.elka.pw.edu.pl>
2021-05-19 13:25:10 +0200

    added checking if leaders are absent

--- include/Node.h
+++ include/Node.h
@@ -10,14 +10,16 @@
 
 #include <sys/socket.h>
 #include <thread>
+#include <chrono>
 #include "Logger.h"
 #include "Receiver.h"
 #include "Sender.h"
 
 #define PORT 6000
+#define MAX_WAITING_TIME 3000      //3 seconds for debugging purposes, in future 0.1 s
 #define LEADERS_MESSAGE 'a'
-#define IDS_MESSAGE 'b'
-#define ROLE_POSITION 4
+#define ID_MESSAGE 'b'
+#define ROLE_POSITION 2
 #define NONE 0
 #define LEADER 1
 #define VICE_LEADER 2
@@ -30,6 +32,10 @@ private:
     int sock;
     int id;
     int role;
+    time_t last_leader_msg_time;
+    time_t last_v_leader_msg_time;
+    mutable std::mutex vl_mutex;
+    mutable std::mutex l_mutex;
 
 public:
     Node(int, int);
@@ -40,14 +46,31 @@ public:
     void init();
 
     /**
+     * @brief method checks if the leader is absent
+     * @return true if absent
+     */
+    bool is_leader_absent();
+
+    /**
+     * @brief method checks if the vice-leader is absent
+     * @return true if absent
+     */
+    bool is_v_leader_absent();
+
+    /**
      * @brief method receiving messages in a loop
      */
-    void *rcvr(void*);
+    void *receiver(void*);
 
     /**
      * @brief method sends messages in a loop
      */
-    void *sndr(void*);
+    void *sender(void*);
+
+    /**
+     * @brief method checks absence of leaders in a loop
+     */
+     //void *absence_checker(void*);        //is it necessary to check absence more often then sender sends messages?
 };
 
 #endif
--- src/Node.cpp
+++ src/Node.cpp
@@ -4,6 +4,8 @@ Node::Node(int id, int role){
     this->sock = 0;
     this->id = id;
     this->role = role;
+    time(&last_leader_msg_time);
+    time(&last_v_leader_msg_time);
 }
 
 void Node::init(){
@@ -23,15 +25,43 @@ void Node::init(){
         exit(1);
     }
 
-    std::thread rcvr_t(&Node::rcvr, this, (void*) nullptr);
-    std::thread sndr_t(&Node::sndr, this, (void*) nullptr);
+    std::thread rcvr_t(&Node::receiver, this, (void*) nullptr);
+    std::thread sndr_t(&Node::sender, this, (void*) nullptr);
+    //std::thread absnc_chckr_t(&Node::absence_checker, this, (void*) nullptr);
 
     rcvr_t.join();
     sndr_t.join();
+    //absnc_chckr_t.join();
     close(sock);
 }
 
-void *Node::rcvr(void* arg){
+bool Node::is_leader_absent() {
+    std::stringstream log_msg;
+    time_t now;
+    std::lock_guard guard(l_mutex);
+    time(&now);
+    if(difftime(now, last_leader_msg_time) * 1000 > MAX_WAITING_TIME){
+        log_msg << "node " << id << " informs: there is not a leader";
+        Logger::getInstance().log(log_msg);
+        return true;
+    }
+    return false;
+}
+
+bool Node::is_v_leader_absent(){
+    std::stringstream log_msg;
+    time_t now;
+    std::lock_guard guard(vl_mutex);
+    time(&now);
+    if(difftime(now, last_v_leader_msg_time) * 1000 > MAX_WAITING_TIME){
+        log_msg << "node " << id << " informs: there is not a vice-leader";
+        Logger::getInstance().log(log_msg);
+        return true;
+    }
+    return false;
+}
+
+void *Node::receiver(void* arg){
     std::stringstream msg;
     Receiver receiver(sock, PORT, IN6ADDR_ANY_INIT);
     receiver.init();
@@ -40,35 +70,66 @@ void *Node::rcvr(void* arg){
     auto v_leader_start = std::chrono::steady_clock::now();
     while(true) {
         receiver.receive(buf, sizeof buf);
-        if(buf[0] = LEADERS_MESSAGE){
-            if(atoi(buf + ROLE_POSITION) == LEADER){       //leader's message received
-                //TODO
+        if(buf[0] == LEADERS_MESSAGE){
+            if(atoi(buf + ROLE_POSITION) == LEADER){                //leader's message received
+                std::lock_guard guard(l_mutex);
+                time(&last_leader_msg_time);
             }else{
-                if(atoi(buf + ROLE_POSITION) == VICE_LEADER){    //vice-leader's message received
-                    //TODO
+                if(atoi(buf + ROLE_POSITION) == VICE_LEADER){       //vice-leader's message received
+                    std::lock_guard guard(vl_mutex);
+                    time(&last_v_leader_msg_time);
                 }else {
                     perror("role in message doesn't exist");
                     exit(1);
                 }
             }
         }
+        if(buf[0] == ID_MESSAGE){
+            //TODO
+        }
         msg << "node " << id << " received: " << buf;
         Logger::getInstance().log(msg);
     }
 }
 
-void *Node::sndr(void *arg){
+void *Node::sender(void *arg){
     Sender sender(sock, PORT);
+    bool leader_absent = false;
+    bool v_leader_absent = false;
     std::stringstream log_msg;
     char msg[MAX_MSG_SIZE];
-    char type_of_message = LEADERS_MESSAGE; // TODO Mock, in future we will need other types of messages
+    char msg_type;
     while(true) {
-        sleep(2);
-        if(role != NONE){
-            sprintf(msg, "%c %d %d", type_of_message, id, role);
+        sleep(1);
+        if(role != NONE){                               //if I have role - sending LEADERS_MESSAGE with role
+            msg_type = LEADERS_MESSAGE;
+            sprintf(msg, "%c %d", msg_type, role);
             sender.send(msg, sizeof msg);
             log_msg << "node " << id << " sent " << msg;
             Logger::getInstance().log(log_msg);
         }
+        if(leader_absent && role == VICE_LEADER) {      //if leader absent and I am a vice-leader - I become a leader
+            role = LEADER;
+            log_msg <<"node" << id << " is now leader" << msg;
+            Logger::getInstance().log(log_msg);
+        }
+        if(v_leader_absent && role != LEADER){          //if vice-leader absent and I am not a leader - sending ID_MESSAGE
+            msg_type = ID_MESSAGE;
+            sprintf(msg, "%c %d", msg_type, id);
+            sender.send(msg, sizeof msg);
+            log_msg <<"node" << id << " sent " << msg;
+            Logger::getInstance().log(log_msg);
+        }
+        leader_absent = is_leader_absent();
+        v_leader_absent = is_v_leader_absent();
     }
 }
+
+/*void *Node::absence_checker(void *arg){
+    while(true){
+        usleep(100000);
+        is_leader_absent();
+        is_v_leader_absent();
+        ...
+    }
+}*/
